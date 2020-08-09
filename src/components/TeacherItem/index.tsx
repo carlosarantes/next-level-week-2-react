28f001167057c5bae8b11f4e260482a42510762a
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-08-09 18:02:43 -0300

    implementações

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,33 +2,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem () {
+export interface Teacher {
+    id : number;
+    avatar : string;
+    bio : string;
+    cost : number;
+    name : string;
+    subject : string;
+    whatsapp : string;
+}
+
+interface TeacherItemProps {
+    teacher : Teacher;
+}
+
+const TeacherItem : React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id : teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://veja.abril.com.br/wp-content/uploads/2017/12/tiririca-despedida.jpg?quality=70&strip=info&w=680&h=453&crop=1" alt="Abestado" />
+            <img src={teacher.avatar} alt={teacher.name} />
             <div>
-                <strong>Tiririca</strong>
-                <span>Ciências Filosóficas</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
 
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, omnis, <br/><br/>
-        porro quod quo praesentium, est accusantium culpa tenetur reprehenderit repudiandae fugiat sed 
-        natus voluptatem. Autem nesciunt expedita cumque dolores doloremque.
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
             <p>
                 Preço/Hora 
-                <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a target="_blank" onClick={createNewConnection}  href={ `http://wa.me/${teacher.whatsapp}` }>
                 <img src={whatsappIcon} alt="Entrar em contato" />
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     );
